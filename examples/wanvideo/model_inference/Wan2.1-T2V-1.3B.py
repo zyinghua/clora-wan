c3f662f1e6688bbe4504f307b2d0976226871da2
zyinghua <49605250+zyinghua@users.noreply.github.com>
2026-04-16 17:42:07 -0400

    remove vram management

--- examples/wanvideo/model_inference/Wan2.1-T2V-1.3B.py
+++ examples/wanvideo/model_inference/Wan2.1-T2V-1.3B.py
@@ -13,8 +13,6 @@ pipe = WanVideoPipeline.from_pretrained(
         ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu", local_model_path="/root/autodl-tmp/models/clora-wan"),
     ],
 )
-pipe.enable_vram_management()
-
 # Text-to-video
 video = pipe(
     prompt="纪实摄影风格画面，一只活泼的小狗在绿茵茵的草地上迅速奔跑。小狗毛色棕黄，两只耳朵立起，神情专注而欢快。阳光洒在它身上，使得毛发看上去格外柔软而闪亮。背景是一片开阔的草地，偶尔点缀着几朵野花，远处隐约可见蓝天和几片白云。透视感鲜明，捕捉小狗奔跑时的动感和四周草地的生机。中景侧面移动视角。",
