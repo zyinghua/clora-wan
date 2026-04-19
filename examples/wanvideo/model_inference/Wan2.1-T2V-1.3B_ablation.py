1060783856462cb3dd5f2e1e206ea43d271ce17e
zyinghua <49605250+zyinghua@users.noreply.github.com>
2026-04-18 23:35:40 -0400

    ablation script update

--- examples/wanvideo/model_inference/Wan2.1-T2V-1.3B_ablation.py
+++ examples/wanvideo/model_inference/Wan2.1-T2V-1.3B_ablation.py
@@ -8,21 +8,35 @@ pipe = WanVideoPipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
     device="cuda",
     model_configs=[
-        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="diffusion_pytorch_model*.safetensors", offload_device="cpu", local_model_path="/root/autodl-tmp/models/clora-wan"),
-        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="models_t5_umt5-xxl-enc-bf16.pth", offload_device="cpu", local_model_path="/root/autodl-tmp/models/clora-wan"),
-        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu", local_model_path="/root/autodl-tmp/models/clora-wan"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="diffusion_pytorch_model*.safetensors", offload_device="cpu", local_model_path="/workspace/autodl-tmp/models/clora-wan"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="models_t5_umt5-xxl-enc-bf16.pth", offload_device="cpu", local_model_path="/workspace/autodl-tmp/models/clora-wan"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-T2V-1.3B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu", local_model_path="/workspace/autodl-tmp/models/clora-wan"),
     ],
 )
-# Text-to-video with prompt ablation
-video = pipe(
-    prompt="A dog running on grass.",
-    negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
-    ablation_prompt="A cat running on grass.",
-    ablation_block_size=5,
-    ablation_block_id=1,
-    seed=None, tiled=True,
-)
-save_video(video, "video_wan_out.mp4", fps=15, quality=5)
+# Text-to-video with prompt ablation (search over block id × block size)
+PROMPT = "A video shot of a tiger."
+NEGATIVE_PROMPT = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
+ABLATION_PROMPT = "A video shot of a bunny."
+
+ablation_range = {
+    5: range(1, 7),
+    10: range(1, 4),
+    3: range(1, 11),
+} # stride: block id
+
+for ablation_block_size in ablation_range.keys():
+    for ablation_block_id in ablation_range[ablation_block_size]:
+        video = pipe(
+            prompt=PROMPT,
+            negative_prompt=NEGATIVE_PROMPT,
+            ablation_prompt=ABLATION_PROMPT,
+            ablation_block_size=ablation_block_size,
+            ablation_block_id=ablation_block_id,
+            seed=None,
+            tiled=True,
+        )
+        out_name = f"video_wan_out_ablation_{ablation_block_size}-{ablation_block_id}.mp4"
+        save_video(video, out_name, fps=15, quality=5)
 
 # # Video-to-video
 # video = VideoData("video1.mp4", height=480, width=832)
